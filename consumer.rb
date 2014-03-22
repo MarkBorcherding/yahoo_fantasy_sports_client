ae66726482d38699b41f512f8d17178bf205a73d
Mark Borcherding <markborcherding@gmail.com>
2014-03-21 20:53:56 -0500

    do more gets

--- consumer.rb
+++ consumer.rb
@@ -4,6 +4,7 @@ require 'highline/import'
 require 'dotenv'
 require 'awesome_print'
 Dotenv.load
+require 'fileutils'
 
 options = {
   :site                 => 'https://api.login.yahoo.com',
@@ -19,19 +20,34 @@ consumer=OAuth::Consumer.new(
   ENV['consumer_secret'],
   options)
 
-access_token_text = ask('Enter current token: ')
-access_secret_text = ask('Enter current secret: ')
-if access_token_text == '' || access_secret_text == ''
+access_token_text = IO.read('.access_token') if File.exist? '.access_token'
+access_secret_text = IO.read('.access_secret') if File.exist? '.access_secret'
+unless access_token_text && access_secret_text
   request_token = consumer.get_request_token
   puts "Visit #{request_token.authorize_url}"
   access_token_text = ask('Enter the token:')
-  access_token = request_token.get_access_token :oauth_verifier => access_token_text
+  @access_token = request_token.get_access_token :oauth_verifier => access_token_text
+  FileUtils.rm '.access_token', force: true
+  File.open('.access_token','w') { |f| f.write(@access_token.token) }
+  FileUtils.rm '.access_secret', force: true
+  File.open('.access_secret','w') { |f| f.write(@access_token.secret) }
 else
-  access_token = OAuth::AccessToken.new(consumer, access_token_text, access_secret_text)
+  @access_token = OAuth::AccessToken.new(consumer, access_token_text, access_secret_text)
 end
 
 
-response = access_token.get "http://fantasysports.yahooapis.com/fantasy/v2/game/nfl"
-ap response.body
+def get(url)
+  response = @access_token.get "http://fantasysports.yahooapis.com#{url}"
+  ap response.body
+  puts "*" * 20
+end
+
+get "/fantasy/v2/users;use_login=1/games;game_keys=mlb"
+
+get "/fantasy/v2/game/328"
+
+get "/fantasy/v2/users;use_login=1/games;game_keys=328/leagues"
+
+get "/fantasy/v2/league/328.l.46539/players"
 
 
